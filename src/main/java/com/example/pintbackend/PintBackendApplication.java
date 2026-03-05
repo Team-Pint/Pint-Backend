1c4aab80d30b7c3352eb6dd1db1f01473fba1b34
Junsung Kim <uitomde@gmail.com>
2026-03-05 16:41:56 +0900

    초반 틀

---
+++ src/main/java/com/example/pintbackend/PintBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pintbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PintBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PintBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pintbackend/PintBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pintbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PintBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
