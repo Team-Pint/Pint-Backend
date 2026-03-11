7d1633c108143c29a338a0844418f13e62769893
Junsung Kim <uitomde@gmail.com>
2026-03-11 12:32:14 +0900

    update build.gradle

---
+++ src/main/java/com/example/pintbackend/aop/ApiLoggingAspect.java
@@ -0,0 +1,14 @@
+/**
+ * File: null.java
+ * Path: com.example.pintbackend.aop
+ *
+ * Outline:
+ * using AOP to measure how long it takes to execute an api request
+ *
+ * Author: jskt
+ */
+
+package com.example.pintbackend.aop;
+
+public class ApiLoggingAspect {
+}
---
+++ src/main/java/com/example/pintbackend/dto/postDto/UpdatePostRequest.java
@@ -0,0 +1,14 @@
+/**
+ * File: null.java
+ * Path: com.example.pintbackend.dto.postDto
+ *
+ * Outline:
+ * dto for update post request
+ *
+ * Author: jskt
+ */
+
+package com.example.pintbackend.dto.postDto;
+
+public class UpdatePostRequest {
+}
